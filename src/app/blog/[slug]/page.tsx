6a0e14e1e2bb2d4f11401d8548da469e92597814
MD-ALL-SHAHRIA <mdallshahria@gmail.com>
2026-01-09 20:53:27 +0600

    Full Functional Blogpage

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import BlogSidebar from "@/components/BlogSidebar";
 
 import {
   Calendar,
@@ -278,6 +279,8 @@ export default function BlogPost() {
               </div>
             </div>
           </article>
+
+          <BlogSidebar />
 
          
           
